6b23a3f2125b78ef7e66d6475662d1cef8c23739
Overgrown <overgrownmc@gmail.com>
2025-11-24 13:26:43 -0500

    Fix #18
    Added models and textures to the Aetheric Goggles and Basic Casting Gauntlet. However, only added models to the primary aspect cluster blocks. Textures will be added eventually but they exist now... kinda

--- src/main/java/dev/overgrown/thaumaturge/ThaumaturgeDataGenerator.java
+++ src/main/java/dev/overgrown/thaumaturge/ThaumaturgeDataGenerator.java
@@ -1,6 +1,5 @@
 package dev.overgrown.thaumaturge;
 
-import com.klikli_dev.modonomicon.datagen.ItemTagsProvider;
 import dev.overgrown.thaumaturge.data_generator.ModBlockLootTables;
 import dev.overgrown.thaumaturge.data_generator.ModBlockTags;
 import dev.overgrown.thaumaturge.data_generator.ModItemTags;
@@ -8,11 +7,11 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ThaumaturgeDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(ModBlockLootTables::new);
         pack.addProvider(ModBlockTags::new);
         pack.addProvider(ModItemTags::new);
-	}
+    }
 }
--- src/main/java/dev/overgrown/thaumaturge/item/gauntlet/BasicCastingGauntletItem.java
+++ src/main/java/dev/overgrown/thaumaturge/item/gauntlet/BasicCastingGauntletItem.java
@@ -1,7 +1,44 @@
 package dev.overgrown.thaumaturge.item.gauntlet;
 
-public class BasicCastingGauntletItem extends ResonanceGauntletItem {
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.DyeableItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.trim.ArmorTrim;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BasicCastingGauntletItem extends ResonanceGauntletItem implements DyeableItem {
     public BasicCastingGauntletItem(Settings settings) {
         super(settings, 1);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+
+        // Add trim tooltip if present
+        if (world != null) {
+            ArmorTrim.getTrim(world.getRegistryManager(), stack).ifPresent(trim -> ArmorTrim.appendTooltip(stack, world.getRegistryManager(), tooltip));
+        }
+    }
+
+    // DyeableItem implementation
+    @Override
+    public int getColor(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getSubNbt("display");
+        return nbtCompound != null && nbtCompound.contains("color", NbtElement.NUMBER_TYPE)
+                ? nbtCompound.getInt("color")
+                : 0xA06540; // Default leather color
+    }
+
+    // Override to handle custom default color
+    @Override
+    public void setColor(ItemStack stack, int color) {
+        stack.getOrCreateSubNbt("display").putInt("color", color);
     }
 }
